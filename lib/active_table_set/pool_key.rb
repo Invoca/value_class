ce1598e1140e8146825dbecd33b9920c82ec48a6
Victor Borda <brytegeist@gmail.com>
2015-03-30 17:23:16 -0700

    Renaming PoolKey field from ip_address to host

--- lib/active_table_set/pool_key.rb
+++ lib/active_table_set/pool_key.rb
@@ -4,18 +4,18 @@ module ActiveTableSet
   class PoolKey
     include ActiveTableSet::ModelComparison
 
-    attr_accessor :ip_address, :username, :password, :timeout
+    attr_accessor :host, :username, :password, :timeout
 
-    def initialize(ip_address: nil, username: nil, password: nil, timeout: nil)
-      ip_address or raise "Must provide an ip_address"
-      username   or raise "Must provide a username"
-      password   or raise "Must provide a password"
-      timeout    or raise "Must provide a timeout"
+    def initialize(host: nil, username: nil, password: nil, timeout: nil)
+      host     or raise "Must provide a host"
+      username or raise "Must provide a username"
+      password or raise "Must provide a password"
+      timeout  or raise "Must provide a timeout"
 
-      @ip_address = ip_address
-      @username   = username
-      @password   = password
-      @timeout    = timeout
+      @host     = host
+      @username = username
+      @password = password
+      @timeout  = timeout
     end
   end
 end
--- spec/active_table_set/pool_key_spec.rb
+++ spec/active_table_set/pool_key_spec.rb
@@ -8,28 +8,28 @@ describe ActiveTableSet::PoolKey do
     let(:timeout)  { 5 }
 
     it "takes database ip, username, password, and timeout as params" do
-      key = ActiveTableSet::PoolKey.new(ip_address: ip, username: username, password: password, timeout: timeout)
+      key = ActiveTableSet::PoolKey.new(host: ip, username: username, password: password, timeout: timeout)
 
-      expect(key.ip_address).to eq(ip)
+      expect(key.host).to eq(ip)
       expect(key.username).to eq(username)
       expect(key.password).to eq(password)
       expect(key.timeout).to eq(timeout)
     end
 
-    it "raises if not passed an ip_address" do
-      expect { ActiveTableSet::PoolKey.new(username: username, password: password, timeout: timeout) }.to raise_error(RuntimeError, "Must provide an ip_address")
+    it "raises if not passed an host" do
+      expect { ActiveTableSet::PoolKey.new(username: username, password: password, timeout: timeout) }.to raise_error(RuntimeError, "Must provide a host")
     end
 
     it "raises if not passed a username" do
-      expect { ActiveTableSet::PoolKey.new(ip_address: ip, password: password, timeout: timeout) }.to raise_error(RuntimeError, "Must provide a username")
+      expect { ActiveTableSet::PoolKey.new(host: ip, password: password, timeout: timeout) }.to raise_error(RuntimeError, "Must provide a username")
     end
 
     it "raises if not passed a password" do
-      expect { ActiveTableSet::PoolKey.new(ip_address: ip, username: username, timeout: timeout) }.to raise_error(RuntimeError, "Must provide a password")
+      expect { ActiveTableSet::PoolKey.new(host: ip, username: username, timeout: timeout) }.to raise_error(RuntimeError, "Must provide a password")
     end
 
     it "raises if not passed a timeout" do
-      expect { ActiveTableSet::PoolKey.new(ip_address: ip, username: username, password: password) }.to raise_error(RuntimeError, "Must provide a timeout")
+      expect { ActiveTableSet::PoolKey.new(host: ip, username: username, password: password) }.to raise_error(RuntimeError, "Must provide a timeout")
     end
   end
 
@@ -40,32 +40,32 @@ describe ActiveTableSet::PoolKey do
     let(:timeout)  { 5 }
 
     it "should consider two keys equal if ip, username, password, and timeout all match" do
-      key1 = ActiveTableSet::PoolKey.new(ip_address: ip, username: username, password: password, timeout: timeout)
-      key2 = ActiveTableSet::PoolKey.new(ip_address: ip, username: username, password: password, timeout: timeout)
+      key1 = ActiveTableSet::PoolKey.new(host: ip, username: username, password: password, timeout: timeout)
+      key2 = ActiveTableSet::PoolKey.new(host: ip, username: username, password: password, timeout: timeout)
       expect(key1).to eq(key2)
     end
 
-    it "should consider two keys not equal if ip_addresses do not match" do
-      key1 = ActiveTableSet::PoolKey.new(ip_address: ip, username: username, password: password, timeout: timeout)
-      key2 = ActiveTableSet::PoolKey.new(ip_address: "127.0.0.2", username: username, password: password, timeout: timeout)
+    it "should consider two keys not equal if hostes do not match" do
+      key1 = ActiveTableSet::PoolKey.new(host: ip, username: username, password: password, timeout: timeout)
+      key2 = ActiveTableSet::PoolKey.new(host: "127.0.0.2", username: username, password: password, timeout: timeout)
       expect(key1).not_to eq(key2)
     end
 
     it "should consider two keys not equal if usernames do not match" do
-      key1 = ActiveTableSet::PoolKey.new(ip_address: ip, username: username, password: password, timeout: timeout)
-      key2 = ActiveTableSet::PoolKey.new(ip_address: ip, username: "something", password: password, timeout: timeout)
+      key1 = ActiveTableSet::PoolKey.new(host: ip, username: username, password: password, timeout: timeout)
+      key2 = ActiveTableSet::PoolKey.new(host: ip, username: "something", password: password, timeout: timeout)
       expect(key1).not_to eq(key2)
     end
 
     it "should consider two keys not equal if passwords do not match" do
-      key1 = ActiveTableSet::PoolKey.new(ip_address: ip, username: username, password: password, timeout: timeout)
-      key2 = ActiveTableSet::PoolKey.new(ip_address: ip, username: username, password: "something", timeout: timeout)
+      key1 = ActiveTableSet::PoolKey.new(host: ip, username: username, password: password, timeout: timeout)
+      key2 = ActiveTableSet::PoolKey.new(host: ip, username: username, password: "something", timeout: timeout)
       expect(key1).not_to eq(key2)
     end
 
     it "should consider two keys not equal if timeouts do not match" do
-      key1 = ActiveTableSet::PoolKey.new(ip_address: ip, username: username, password: password, timeout: timeout)
-      key2 = ActiveTableSet::PoolKey.new(ip_address: ip, username: username, password: password, timeout: 6)
+      key1 = ActiveTableSet::PoolKey.new(host: ip, username: username, password: password, timeout: timeout)
+      key2 = ActiveTableSet::PoolKey.new(host: ip, username: username, password: password, timeout: 6)
       expect(key1).not_to eq(key2)
     end
   end
