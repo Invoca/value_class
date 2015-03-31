4a176d8ea75e7989f82f5f20dc3b83a94dcac9c9
Victor Borda <brytegeist@gmail.com>
2015-03-30 17:23:45 -0700

    Updating spec/helper to include new classes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,5 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'active_table_set'
 require 'active_table_set/database_server'
 require 'active_table_set/pool_key'
+require 'active_table_set/database_config'
+require 'active_table_set/model_comparison'
