61021ef8a198c6f6fd6ffc2e203c126a947a1739
Victor Borda <brytegeist@gmail.com>
2015-04-02 14:23:02 -0700

    Adding deep clone and timeout reset to PoolKey

--- lib/active_table_set/pool_key.rb
+++ lib/active_table_set/pool_key.rb
@@ -4,8 +4,9 @@ module ActiveTableSet
   class PoolKey
     include AttrComparable
 
-    attr_compare :host, :username, :password, :timeout
-    attr_reader  :host, :username, :password, :timeout, :config
+    attr_compare  :host, :username, :password, :timeout
+    attr_reader   :host, :username, :password, :timeout
+    attr_accessor :config
 
     def initialize(host:, username:, password:, timeout:, config:)
       @host     = host
@@ -13,6 +14,18 @@ module ActiveTableSet
       @password = password
       @timeout  = timeout
       @config   = config
+    end
+
+    def clone_with_new_timeout(timeout)
+      copy = self.clone
+      copy.config = self.config.clone
+      copy.reset_timeout(timeout)
+      copy
+    end
+
+    def reset_timeout(timeout)
+      @timeout = timeout
+      @config.timeout = timeout
     end
   end
 end
--- spec/active_table_set/pool_key_spec.rb
+++ spec/active_table_set/pool_key_spec.rb
@@ -5,7 +5,7 @@ describe ActiveTableSet::PoolKey do
   let(:username) { "test_user" }
   let(:password) { "test_password" }
   let(:timeout)  { 5 }
-  let(:config)   { ActiveTableSet::DatabaseConfig.new }
+  let(:config)   { ActiveTableSet::DatabaseConfig.new(timeout: timeout) }
 
   context "constructor" do
     it "takes database ip, username, password, and timeout as params" do
@@ -35,34 +35,47 @@ describe ActiveTableSet::PoolKey do
   end
 
   context "comparison" do
-    it "should consider two keys equal if ip, username, password, and timeout all match" do
+    it "considers two keys equal if ip, username, password, and timeout all match" do
       key1 = ActiveTableSet::PoolKey.new(host: ip, username: username, password: password, timeout: timeout, config: config)
       key2 = ActiveTableSet::PoolKey.new(host: ip, username: username, password: password, timeout: timeout, config: config)
       expect(key1).to eq(key2)
     end
 
-    it "should consider two keys not equal if hostes do not match" do
+    it "considers two keys not equal if hostes do not match" do
       key1 = ActiveTableSet::PoolKey.new(host: ip, username: username, password: password, timeout: timeout, config: config)
       key2 = ActiveTableSet::PoolKey.new(host: "127.0.0.2", username: username, password: password, timeout: timeout, config: config)
       expect(key1).not_to eq(key2)
     end
 
-    it "should consider two keys not equal if usernames do not match" do
+    it "considers two keys not equal if usernames do not match" do
       key1 = ActiveTableSet::PoolKey.new(host: ip, username: username, password: password, timeout: timeout, config: config)
       key2 = ActiveTableSet::PoolKey.new(host: ip, username: "something", password: password, timeout: timeout, config: config)
       expect(key1).not_to eq(key2)
     end
 
-    it "should consider two keys not equal if passwords do not match" do
+    it "considers two keys not equal if passwords do not match" do
       key1 = ActiveTableSet::PoolKey.new(host: ip, username: username, password: password, timeout: timeout, config: config, config: config)
       key2 = ActiveTableSet::PoolKey.new(host: ip, username: username, password: "something", timeout: timeout, config: config)
       expect(key1).not_to eq(key2)
     end
 
-    it "should consider two keys not equal if timeouts do not match" do
+    it "considers two keys not equal if timeouts do not match" do
       key1 = ActiveTableSet::PoolKey.new(host: ip, username: username, password: password, timeout: timeout, config: config)
       key2 = ActiveTableSet::PoolKey.new(host: ip, username: username, password: password, timeout: 6, config: config)
       expect(key1).not_to eq(key2)
+    end
+  end
+
+  context "clone and reset timeout" do
+    it "cleanly clones itself and its associated config" do
+      key1 = ActiveTableSet::PoolKey.new(host: ip, username: username, password: password, timeout: timeout, config: config)
+      key2 = key1.clone_with_new_timeout(15)
+
+      expect(key1.config.timeout).to eq(timeout)
+      expect(key1.timeout).to eq(timeout)
+
+      expect(key2.config.timeout).to eq(15)
+      expect(key2.timeout).to eq(15)
     end
   end
 end
