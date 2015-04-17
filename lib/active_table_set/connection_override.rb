309b327a8b4ee5cbf6d2bb15e0fc0121d61fbed9
Victor Borda <brytegeist@gmail.com>
2015-04-17 14:15:52 -0700

    Adding ConnectionOverride to override AREL connection

---
+++ lib/active_table_set/connection_override.rb
@@ -0,0 +1,28 @@
+require 'active_record'
+require 'active_table_set/default_config_loader'
+
+# Overrides ActiveRecord::Base
+# include ConnectionOverride in your ApplicationModel class
+module ActiveTableSet
+  module ConnectionOverride
+    extend ActiveSupport::Concern
+
+    module ClassMethods
+      def ats_config
+        ActiveTableSet::DefaultConfigLoader.new
+      end
+
+      def connection
+        @@connection_proxy ||= proxy_with_default_table_set
+      end
+
+      def proxy_with_default_table_set
+        proxy = ConnectionProxy.new(config: ats_config.configuration)
+        proxy.set_default_table_set(table_set_name: "common")
+        proxy
+      end
+    end
+  end
+end
+
+ActiveRecord::Base.send :include, ActiveTableSet::ConnectionOverride
---
+++ spec/active_table_set/connection_override_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'active_record'
+
+describe ActiveTableSet::ConnectionOverride do
+  context "AREL injection" do
+
+    class TestDummy
+      include ActiveRecord
+      include ActiveTableSet::ConnectionOverride
+    end
+
+    it "overloads ActiveRecord::Base.connection to return a ConnectionProxy" do
+      connection = TestDummy.connection
+      expect(connection.is_a?(ActiveTableSet::ConnectionProxy)).to eq(true)
+    end
+  end
+end
