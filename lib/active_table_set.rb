31b3142974d120c4504a77c3107b036426af328e
Victor Borda <brytegeist@gmail.com>
2015-04-21 17:19:14 -0700

    Rearranging requires

--- lib/active_table_set.rb
+++ lib/active_table_set.rb
@@ -1,9 +1,4 @@
 require "active_table_set/version"
-
-require 'yaml'
-require 'active_support/core_ext'
-require 'active_support/hash_with_indifferent_access'
-
 require 'active_table_set/table_set_config'
 require 'active_table_set/database_config'
 require 'active_table_set/partition_config'
@@ -11,6 +6,13 @@ require 'active_table_set/pool_manager'
 require 'active_table_set/pool_key'
 require 'active_table_set/partition'
 require 'active_table_set/table_set'
+require 'active_table_set/connection_proxy'
+require 'active_table_set/connection_override'
+require 'active_table_set/default_config_loader'
+
+require 'yaml'
+require 'active_support/core_ext'
+require 'active_support/hash_with_indifferent_access'
 
 module ActiveTableSet
 end
