1c0589bfe3145ba0acc23ca13d76f9c9d727b570
Bob Smith <bob@invoca.com>
2015-05-26 10:19:11 -0700

    tech-509 add config class and spec

---
+++ lib/active_table_set/config.rb
@@ -0,0 +1,14 @@
+module ActiveTableSet
+  class Config
+    include ValueClass::Constructable
+
+    value_attr      :enforce_access_policy
+    value_attr      :environment
+
+    # TODO
+    # - How to specify default database attributes
+    # - How to specify default connection attributes?
+
+    value_list_attr :table_sets,     class_name: 'ActiveTableSet::TableSet', insert_method: :table_set
+  end
+end
---
+++ spec/active_table_set/config_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe ActiveTableSet::Config do
+
+  it "can be constructed from a block" do
+    ats_config = ActiveTableSet::Config.config do |conf|
+      conf.enforce_access_policy = true
+      conf.environment = 'test'
+
+      conf.table_set do |ts|
+        ts.name = :common
+
+        ts.access_policy do |ap|
+          ap.disallow_read = 'cf_%'
+        end
+
+        ts.partition do |part|
+          part.leader do |leader|
+            leader.host = "127.0.0.8"
+            leader.username = "tester"
+            leader.password = "verysecure"
+            leader.timeout  = 2
+            leader.database ="main"
+          end
+
+          part.follower do |follower|
+            follower.host = "127.0.0.9"
+            follower.username = "tester1"
+            follower.password = "verysecure1"
+            follower.timeout  = 2
+            follower.database ="replication1"
+          end
+
+          part.follower do |follower|
+            follower.host = "127.0.0.10"
+            follower.username = "tester2"
+            follower.password = "verysecure2"
+            follower.timeout  = 2
+            follower.database ="replication2"
+          end
+        end
+      end
+    end
+  end
+end
