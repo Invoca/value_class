ffd49260fa94b7ec93ffd24e0d5d15f798981eb9
Bob Smith <bob@invoca.com>
2015-05-31 16:40:41 -0700

    tech-509 - iterating on remote branch

--- lib/active_table_set/pool_key.rb
+++ lib/active_table_set/pool_key.rb
@@ -1,17 +1,15 @@
 module ActiveTableSet
   # these match the active record connection attributes.
-  PoolKey = ValueClass.struct(
-    :host,
-    :database,
-    :username,
-    :password,
-    :connect_timeout,
-    :read_timeout,
-    :write_timeout,
-    :encoding,
-    :collation,
-    :adapter,
-    :pool,
-    :reconnect)
+  class PoolKey < ValueClass.struct(
+    :host, :database, :username, :password, :connect_timeout, :read_timeout,
+    :write_timeout, :encoding, :collation, :adapter, :pool, :reconnect)
+
+
+    # TODO - need test
+    def name
+      "#{host}_#{database}_#{username}_#{read_timeout}"
+    end
+  end
+
 end
 
