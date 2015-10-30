a3983a0d8356cdf47c6b92a1d25b366eb719631a
Nicholas Ellis <nellis@invoca.com>
2015-10-30 12:18:30 -0700

    TECH-696: Adding test

--- spec/active_table_set/extensions/connection_extension_spec.rb
+++ spec/active_table_set/extensions/connection_extension_spec.rb
@@ -11,6 +11,28 @@ module ConnectionExtensionSpec
 end
 
 describe ActiveTableSet::Extensions::ConnectionExtension do
+  context "log checking" do
+    it "adds the host to the log message" do
+      class TestExtensionLogging
+        prepend ActiveTableSet::Extensions::ConnectionExtension
+
+        def initialize(config)
+          @config = config
+        end
+
+        def config
+          @config
+        end
+
+        def log(sql, name, binds)
+          return name
+        end
+      end
+
+      test_log = TestExtensionLogging.new({})
+      expect(test_log.log('', '')).to match(/host:/)
+    end
+  end
   context "ConvientDelegation" do
 
     it "can delegate using to instances" do
