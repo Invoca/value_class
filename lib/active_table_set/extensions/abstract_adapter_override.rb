d310ef4eada15b070d87ab3847b7ebc367d8f91f
Omeed Rabani <orabani@nborabani.local>
2016-07-13 11:09:57 -0700

    100 TECH-1150 move mysql adapter patches: make reset_rows_read method not protected

--- lib/active_table_set/extensions/abstract_adapter_override.rb
+++ lib/active_table_set/extensions/abstract_adapter_override.rb
@@ -6,44 +6,44 @@ module ActiveTableSet
         @rows_read = []
       end
 
-      protected
-
-      def connection=(new_connection)
-        @connection = new_connection
-        @connection_set_caller = caller
-      end
-
-      def non_nil_connection
-        @connection or raise "Connection is nil! It was assigned by:\n#{(@connection_set_caller || ['<none>']).join("\n")}"
-      end
-
       def reset_rows_read
         rows_read, @rows_read = @rows_read, []
         rows_read
       end
 
-      def log(sql, name = "SQL", binds = [])
-        @instrumenter.instrument(
-            "sql.active_record",
-            hash = {
-                :sql           => sql,
-                :name          => name,
-                :connection_id => object_id,
-                :binds         => binds
-            }
-        ) do
-          yield.tap do |result|
-            hash[:rows] = result.count if result.respond_to?(:count)
-            hash[:last_id] = @connection.last_id if @connection.respond_to?(:last_id) && @connection.last_id > 0
-          end
+      protected
+
+        def connection=(new_connection)
+          @connection = new_connection
+          @connection_set_caller = caller
         end
-      rescue Exception => e
-        message = "#{e.class.name}: #{e.message}: #{sql}"
-        @logger.debug message if @logger
-        exception = translate_exception(e, message)
-        exception.set_backtrace e.backtrace
-        raise exception
-      end
+
+        def non_nil_connection
+          @connection or raise "Connection is nil! It was assigned by:\n#{(@connection_set_caller || ['<none>']).join("\n")}"
+        end
+
+        def log(sql, name = "SQL", binds = [])
+          @instrumenter.instrument(
+              "sql.active_record",
+              hash = {
+                  :sql           => sql,
+                  :name          => name,
+                  :connection_id => object_id,
+                  :binds         => binds
+              }
+          ) do
+            yield.tap do |result|
+              hash[:rows] = result.count if result.respond_to?(:count)
+              hash[:last_id] = @connection.last_id if @connection.respond_to?(:last_id) && @connection.last_id > 0
+            end
+          end
+        rescue Exception => e
+          message = "#{e.class.name}: #{e.message}: #{sql}"
+          @logger.debug message if @logger
+          exception = translate_exception(e, message)
+          exception.set_backtrace e.backtrace
+          raise exception
+        end
     end
   end
 end
