0d50e4c464ae3b65c9afe5c829a963de196acb31
Omeed Rabani <orabani@nborabani.local>
2016-08-01 10:21:13 -0700

    TECH-1150 move mysql adapter patches: DRY up tests

--- spec/active_table_set/extensions/abstract_mysql_adapter_override_spec.rb
+++ spec/active_table_set/extensions/abstract_mysql_adapter_override_spec.rb
@@ -3,29 +3,7 @@ require 'spec_helper'
 describe ActiveTableSet::Extensions::AbstractMysqlAdapterOverride do
   context "Mysql2AdapterOverride" do
     before :each do
-      ActiveTableSet.config do |conf|
-        conf.enforce_access_policy true
-        conf.environment           'test'
-        conf.default  =  { table_set: :common }
-
-        conf.table_set do |ts|
-          ts.name = :common
-
-          ts.access_policy do |ap|
-            ap.disallow_read  'cf_%'
-            ap.disallow_write 'cf_%'
-          end
-
-          ts.partition do |part|
-            part.leader do |leader|
-              leader.host                 "10.0.0.1"
-              leader.read_write_username  "tester"
-              leader.read_write_password  "verysecure"
-              leader.database             "main"
-            end
-          end
-        end
-      end
+      small_table_set
 
       @connection = StubDbAdaptor.stub_db_connection()
       @connection.extend(ActiveTableSet::Extensions::AbstractMysqlAdapterOverride)
