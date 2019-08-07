99dc36bc4c919e37c96d3df0e394d99b0c5f436e
Alec Jacobs <ajacobs@invoca.com>
2019-08-07 08:07:52 -0700

    TECH-3622 add net_read_timeout to NamedTimeout value class

--- spec/active_table_set/configuration/named_timeout_spec.rb
+++ spec/active_table_set/configuration/named_timeout_spec.rb
@@ -4,18 +4,40 @@ require 'spec_helper'
 
 describe ActiveTableSet::Configuration::NamedTimeout do
   context "named_timeout" do
+    subject(:named_timeout) { ActiveTableSet::Configuration::NamedTimeout.new(name: name, timeout: timeout, net_read_timeout: net_read_timeout) }
+    let(:name) { :web }
+    let(:timeout) { 110.seconds }
+    let(:net_read_timeout) { 10.minutes }
 
     it "can be constructed" do
-      nt = ActiveTableSet::Configuration::NamedTimeout.new(name: :web, timeout: 110.seconds)
-      expect(nt.name).to eq(:web)
-      expect(nt.timeout).to eq(110)
+      expect(named_timeout.name).to eq(:web)
+      expect(named_timeout.timeout).to eq(110)
+      expect(named_timeout.net_read_timeout).to eq(600)
     end
 
-    it "requires arguments" do
-      expect { ActiveTableSet::Configuration::NamedTimeout.new(timeout: 1) }.to raise_error(ArgumentError, "must provide a value for name")
-      expect { ActiveTableSet::Configuration::NamedTimeout.new(name: :web) }.to raise_error(ArgumentError, "must provide a value for timeout")
+    describe "name" do
+      let(:name) { nil }
+
+      it "is required" do
+        expect { named_timeout }.to raise_error(ArgumentError, "must provide a value for name")
+      end
     end
 
+    describe "timeout" do
+      let(:timeout) { nil }
+
+      it "is required" do
+        expect { named_timeout }.to raise_error(ArgumentError, "must provide a value for timeout")
+      end
+    end
+
+    describe "net_read_timeout" do
+      let(:net_read_timeout) { nil }
+
+      it "is optional" do
+        expect(named_timeout.net_read_timeout).to be_nil
+      end
+    end
   end
 end
 
